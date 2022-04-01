19a17ecd5706339bec4fd45c6f99fe9235299fba
Igor <igor.zamaraev@gcorelabs.com>
2022-04-01 18:24:01 +0500

    add git reset in script.

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -2,8 +2,10 @@
 
 echo "Start git pull docker config:"
 cd /opt/memes || exit
+git reset --hard
 git pull
 
 echo "Start git pull application:"
 cd /opt/memes/code || exit
+git reset --hard
 git pull
